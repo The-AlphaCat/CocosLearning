0567ae343ad97ac3473c7c49bcaf0d482e9e2f3a
CuriousApe-AC <a.cyril@ymail.com>
2025-06-20 22:26:09 +0530

    C2

---
+++ mindthestep/assets/Script/PlayerController.ts
@@ -0,0 +1,23 @@
+import { _decorator, Component, EventMouse, Node, systemEvent, SystemEventType } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+    private startJump: boolean = false;
+    private jumpStep: number = 0;
+    private curJumpTime: number = 0;
+    private JumpTime: number = 0.1;
+    private curPos = new
+
+    start() {
+        systemEvent.on(SystemEventType.MOUSE_UP, this.onMouseUp, this);;
+    }
+    onMouseUp(event: EventMouse) {
+
+}
+
+    update(deltaTime: number) {
+        
+    }
+}
+
